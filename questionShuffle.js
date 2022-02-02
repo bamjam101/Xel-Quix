33bc222e21ac8522bb3bfdef86d4d0da7fd25379
bamjam101 <herejamil123@gmail.com>
2022-02-03 01:49:27 +0530

    A dynamic quiz game based on JS, enforced with design and animation <Prototype host>

---
+++ questionShuffle.js
@@ -0,0 +1,126 @@
+let questionCounter = 0;
+let nextCount = 0;
+let score = 0;
+let currentQue;
+let availableQue = [];
+let availableOpt = [];
+let correct;
+const optContainer = document.querySelector(".options");
+const queText = document.querySelector(".question");
+const scored = document.querySelector(".score");
+const button = document.querySelector("button");
+const bubbble = document.querySelector("#bubble");
+
+function setQuestion() {
+    const totalQue = quiz.length;
+    for (let i = 0; i < totalQue; i++) {
+        availableQue.push(quiz[i]);
+    }
+}
+
+function getNewQuestion() {
+    const questionIndex = availableQue[Math.floor(Math.random() * availableQue.length)]
+    currentQue = questionIndex;
+    // Set the question on webpage using innerHTML
+    queText.innerHTML = currentQue.q;
+    // Set respective theme bubble
+    bubble.style.opacity = "0";
+    setTimeout(() => {
+        bubbble.setAttribute("src", currentQue.theme);
+    }, 300);
+    setTimeout(() => {
+        bubble.style.opacity = "30%";
+    }, 1000);
+    // index of current question, position in availableQue array
+    const indexSlice = availableQue.indexOf(questionIndex);
+    // slice out the asked question to avoid repetition
+    availableQue.splice(indexSlice, 1);
+    // optionCode
+    const optionLen = currentQue.option.length;
+
+    for (let i = 0; i < optionLen; i++) {
+        availableOpt.push(i);
+    }
+    optContainer.innerHTML = '';
+    // What is correct option?
+    correct = currentQue.ans;
+    // set the options randomly
+    for (let i = 0; i < optionLen; i++) {
+        const optIndex = availableOpt[Math.floor(Math.random() * availableOpt.length)];
+        const optSlice = availableOpt.indexOf(optIndex);
+        availableOpt.splice(optSlice, 1);
+        const option = document.createElement("div");
+        option.innerHTML = currentQue.option[optIndex];
+        option.id = optIndex;
+        option.className = "box";
+        // option.style.animationDelay = "0.2s";
+        optContainer.appendChild(option);
+        option.setAttribute("onclick", "getResult(this, correct)")
+    }
+    questionCounter++;
+}
+
+function getResult(element, ans) {
+    const optionChose = element.id;
+    if (optionChose == ans) {
+        // styling green, score++, or 
+        element.classList.add("correct");
+        score++;
+        scored.innerHTML = score + " out of 5";
+    } else {
+        // style red
+        element.classList.add("wrong");
+        // To show the correct answer along
+        const optLen = optContainer.children.length;
+        for (let i = 0; i < optLen; i++) {
+            if (optContainer.children[i].id == ans) {
+                optContainer.children[i].classList.add("correct");
+            }
+        }
+    }
+    restrictClick();
+}
+
+function restrictClick() {
+    const optRegionLen = optContainer.children.length;
+    for (let i = 0; i < optRegionLen; i++) {
+        optContainer.children[i].classList.add("already-clicked");
+    }
+}
+
+function next() {
+    nextCount++;
+    queText.style.opacity = "0";
+    setTimeout(() => {
+    queText.style.opacity = "100";
+        
+    if (questionCounter === 5) {
+        menu();
+        queText.style.opacity = "0";
+        button.style.opacity = "0";
+        setTimeout(() => {
+            queText.innerHTML = "You've Finished The Game!";
+            queText.style.opacity = "100";
+            optContainer.innerHTML = "";
+        }, 1000);
+        setTimeout(() => {
+            button.style.opacity = "100";
+        }, 2000);
+        button.innerHTML = "Restart";
+        button.removeAttribute("onclick", "next()");
+        button.setAttribute("onclick", "reload()");
+    } else {
+        getNewQuestion();
+    } 
+    }, 1000);
+
+}
+
+function reload() {
+    window.location.reload();
+}
+
+window.onload = function () {
+    setQuestion();
+    getNewQuestion();
+}
---
+++ script.js
@@ -0,0 +1,158 @@
+let quiz = [
+    {
+        q: "Q. What is the square root of 144?",
+        option: ["12", "7", "11", "16"],
+        ans: 0,
+        theme: "img/math.png"
+    },
+    {
+        q: "Q. What is Earth’s only natural satellite?",
+        option: ["Sun", "Mars", "Moon", "Venus"],
+        ans: 2,
+        theme: "img/earth.png"
+    },
+    {
+        q: "Q. What star shines in the day and provides light?",
+        option: ["Neptune", "Mars", "Sun", "Jupiter"],
+        ans: 2,
+        theme: "img/solar.png"
+    },
+    {
+        q: "Q. What tissue connects muscles to bones?",
+        option: ["Skin", "Blood vessels", "Fat", "Tendon"],
+        ans: 3,
+        theme: "img/connect.png"
+    },
+    {
+        q: "Q. Which nutrient plays an essential role in muscle-building?",
+        option: ["Proteins", "Carbohydrates", "Iron", "Fat"],
+        ans: 1,
+        theme: "img/muscle.png"
+    },
+    {
+        q: "Q. Name the acid that is secreted in the stomach?",
+        option: ["Sulphric acid", "Nitric Acid", "Hydrochloric acid", "Phosphoric acid"],
+        ans: 2,
+        theme: "img/stomach.png"
+    },
+    {
+        q: "Q. Which system of the body controls the senses?",
+        option: ["Circulatory system", "Digestive system", "Skeletal system", "Nervous system"],
+        ans: 3,
+        theme: "img/sense.png"
+    },
+    {
+        q: "Q. Which scientist proposed the three laws of motion?",
+        option: ["Isaac Newton", "Thomas Alva Edison", "Albert Einstein", "Stephen Hawking"],
+        ans: 0,
+        theme: "img/scientist.png"
+    },
+    {
+        q: "Q. Which is the largest animal on earth?",
+        option: ["Shark", "Elephant", "Giraffe", "Blue Whale"],
+        ans: 3,
+        theme: "img/animal.png"
+    }
+];
+
+function curtain() {
+    const curtain = document.querySelector("section");
+    const header = document.querySelector("header");
+    const main = document.querySelector("main");
+    const footer = document.querySelector("footer");
+    setTimeout(() => {
+        footer.style.display = "block";
+        main.style.display = "block";
+        header.style.display = "block";
+    }, 1000);
+    setTimeout(() => {
+        footer.style.opacity = "100";
+        main.style.opacity = "100";
+        header.style.opacity = "100";
+        curtain.style.display = "none";
+    }, 2000);
+    curtain.style.top = "-100%";
+
+    
+    // ask();
+}
+
+let toggle = true;
+
+function menu() {
+    const menu = document.querySelector("nav");
+    const header = document.querySelector("header");
+    const question = document.querySelector(".que");
+    const options = document.querySelector(".options");
+    const menuContent = document.querySelector(".menuContent");
+
+    if (toggle == true) {
+        header.style.height = "6vh";
+        menu.style.position = "absolute";
+        menu.style.top = "0%";
+        menu.style.zIndex = "1000";
+        menu.style.width = "20vw";
+        menu.style.height = "100vh";
+
+        question.style.background = "rgba(0,0,0,0)";
+        options.style.background = "rgba(0,0,0,0)";
+
+        setTimeout(() => {
+            menu.style.background = "rgba(0,0,0,0.5)";
+            menuContent.style.opacity = "100";
+            menuContent.style.zIndex = "1000";
+        }, 1000);
+        return toggle = false;
+    } else {
+        menu.style.position = "none";
+        menu.style.top = "none";
+        menu.style.zIndex = "none";
+        menu.style.width = "100%";
+        menu.style.height = "6vh";
+        menu.style.background = "rgba(0,0,0,0)";
+
+        menuContent.style.opacity = "0";
+        menuContent.style.zIndex = "-1";
+        setTimeout(() => {
+            question.style.background = "rgba(0,0,0,0.2)";
+            options.style.background = "rgba(0,0,0,0.2)";
+        }, 1000);
+
+        return toggle = true;
+    }
+}
+
+// let Counter = 0;
+// let currentQue;
+// let availableQue = [];
+
+// function setQuestion() {
+//     for (let i = 0; i < quiz.length; i++) {
+//         availableQue.push(quiz[i]);
+//     }
+//     console.log(quiz);
+// }
+
+// window.onload = function () {
+//     setQuestion();
+//     getNewQuestion();
+// }
+
+// function ask() {
+//     let score = 0;
+
+//     const questionSlot = document.querySelector(".question");
+//     const opt_1 = document.querySelector(".optA");
+//     const opt_2 = document.querySelector(".optB");
+//     const opt_3 = document.querySelector(".optC");
+//     const opt_4 = document.querySelector(".optD");
+
+//     const quizLen = Quiz.length;
+//     const questionIndex = Math.floor(Math.random() * 19);
+
+//     questionSlot.innerHTML = Quiz.q;
+//     // opt_1.innerHTML = Quiz[questionIndex].option[0];
+//     // opt_2.innerHTML = Quiz[questionIndex].option[1];
+//     // opt_3.innerHTML = Quiz[questionIndex].option[2];
+//     // opt_4.innerHTML = Quiz[questionIndex].option[3];
+// }
